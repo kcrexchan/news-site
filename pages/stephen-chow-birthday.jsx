1df91c9da232e94fd71142b05d4e7d1acfb870b9
Rex Chan <rex@xerex.us>
2026-08-31 10:14:50 -0700

    Add route for Stephen Chow birthday page

---
+++ pages/stephen-chow-birthday.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Head from 'next/head';
+
+export default function Stephen Chow Birthday() {
+  return (
+    <>
+      <Head>
+        <title>生日快樂 Ernest 🎬 · Stephen Chow Birthday</title>
+      </Head>
+      <iframe
+        src="/stephen-chow-birthday.html"
+        style={{ width: '100vw', height: '100vh', border: 'none' }}
+        title="Stephen Chow Birthday"
+      />
+    </>
+  );
+}
